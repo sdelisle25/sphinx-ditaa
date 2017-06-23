52e3716568963ba4d49c44513e810c1718b70394
Sylvain Delisle <sylvain.delisle@supersonicimagine.com>
2017-06-23 14:10:22 +0200

    Add setup.cfg, Fix setup.py

--- setup.py
+++ setup.py
@@ -2,34 +2,43 @@
 
 from setuptools import setup, find_packages
 
-requires = [
+PACKAGES = find_packages()
+
+INSTALL_REQUIRES = [
         'setuptools',
         'Sphinx >=1.0.7',
-    ],
+    ]
 
-setup(
-    name='sphinxcontrib-ditaa',
-    author='Sylvain Delisle',
-    author_email='sylvain.delisle25@gmail.com',
-    url='https://github.com/sdelisle25/sphinxcontrib-ditaa',
-    description='Sphinx contribution plugin for rendering Ditaa diagrams',
-    version='1.0',
-    license="BSD",
-    platforms='any',
-    install_requires=requires,
-    packages=find_packages(),
-    include_package_data=False,
-    namespace_packages=['sphinxcontrib'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
+KEYWORDS = ['sphinx', 'ditaa', 'reST']
+
+CLASSIFIERS = [
+        'Development Status :: 4 - Beta',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
-        'Programming Language :: Python',
+        'Programming Language :: Python 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Documentation',
         'Topic :: Documentation',
         'Topic :: System :: Installation/Setup',
         'License :: OSI Approved :: BSD License',
-    ],
+    ]
 
+PLATEFORMS = 'any'
+LICENSE = 'BSD'
+VERSION = '1.0'
+
+setup(
+    name='sphinxcontrib-ditaa',
+    author='Arthur Gautier',
+    url='https://github.com/baloo/sphinx-ditaa',
+    description='Sphinx contribution plugin for rendering Ditaa diagrams',
+    namespace_packages=['sphinxcontrib'],
+    version=VERSION,
+    license=LICENSE,
+    platforms=PLATEFORMS,
+    packages=PACKAGES,
+    keywords=KEYWORDS,
+    classifiers=CLASSIFIERS,
+    install_requires=INSTALL_REQUIRES,
+    python_requires='>=3.5',
 )
